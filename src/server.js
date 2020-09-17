37c1b21c57ac4a65dbac30a80a0541ab2f89b910
Steven Cho <sxc6061@g.rit.edu>
2020-09-17 16:48:58 -0400

    better ver

--- src/server.js
+++ src/server.js
@@ -1,17 +1,34 @@
 const http = require('http');
+const url = require('url');
+const query = require('querystring');
 const htmlHandler = require('./htmlResponses.js');
 const statusHandler = require('./statusResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const onRequest = (request, response) => {
-  console.log(request.url);
+const urlStruct = {
+  '/': htmlHandler.getIndex,
+  '/success': statusHandler.success,
+  '/badRequest': statusHandler.badRequest,
+  '/unauthorized': statusHandler.unauthorized,
+  '/forbidden': statusHandler.forbidden,
+  '/internal': statusHandler.internal,
+  '/notImplemented': statusHandler.notImplemented,
+  '/badlink': statusHandler.notFound,
+};
 
-  if (request.url == '/'){
-    htmlHandler.getIndex(request, response);
-  }
-  else{
-    statusHandler.getStatus(request,response);
+const onRequest = (request, response) => {
+  // const url = request.url;
+  const parsedUrl = url.parse(request.url);
+  const params = query.parse(parsedUrl.query);
+  const valid = query.parse(parsedUrl.query).valid;
+  console.dir(parsedUrl);
+  console.dir(params);
+  console.dir(valid);
+  if (urlStruct[parsedUrl.pathname]) {
+    urlStruct[parsedUrl.pathname](request, response, params);
+  } else {
+    urlStruct.notFound(request, response, params);
   }
 };
 
--- src/statusResponses.js
+++ src/statusResponses.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const status = [
     {"id" : "Success", "message" : "This is a successful response"},
-    {"id" : "Bad Request","message" : "Missing vlaid query parameter set to true"},
+    {"id" : "Bad Request","message" : "Missing valid query parameter set to true"},
     {"id" : "Unauthorized","message" : "Missing loggedIn query parameter set to yes"},
     {"id" : "Forbidden","message" : "You do not have access to this content"},
     {"id" : "Internal Server Error","message" : "Internal Server Error. Something went wrong."},
@@ -23,6 +23,8 @@ const getStatus = (request,response) =>{
             json = JSON.stringify(status[0])
             break;
         case '/badRequest':
+            res.status(400);
+            res.send('Response');
             json = JSON.stringify(status[1]);
             break;
         case '/unauthorized':
@@ -45,4 +47,88 @@ const getStatus = (request,response) =>{
     response.end();
 }
 
-module.exports.getStatus = getStatus;
+const respondJSON = (request, response, status, object) => {
+    response.writeHead(status, { 'Content-Type': 'application/json' });
+    response.write(JSON.stringify(object));
+    response.end();
+  };
+  
+  const success = (request, response) => {
+    const responseJSON = {
+      message: 'This is a successful response',
+    };
+  
+    respondJSON(request, response, 200, responseJSON);
+  };
+  
+  const badRequest = (request, response, params) => {
+    const responseJSON = {
+      message: 'This request has the required parameters',
+    };
+    
+    if (!params.valid || params.valid != 'true'){ // REMINDER: everything sent over is a **String**, e.g. not a Boolean or a Number or ...
+      responseJSON.message = 'Missing `valid` query parameter set equal to `true`';
+      responseJSON.id = "badRequest";
+      return respondJSON(request, response, 400, responseJSON);
+    }
+    return respondJSON(request, response, 200, responseJSON);
+  };
+  
+  const notFound = (request, response) => {
+    const responseJSON = {
+      message: 'The page you are looking for was not found.',
+      id: 'notFound',
+    };
+    
+    return respondJSON(request, response, 404, responseJSON);
+  };
+
+  const unauthorized = (request, response) => {
+    const responseJSON = {
+        message: 'This request has the required parameters',
+      };
+      
+      if (!params.valid || params.valid != 'true'){ // REMINDER: everything sent over is a **String**, e.g. not a Boolean or a Number or ...
+        responseJSON.message = 'Missing loggedIn query parameter set to yes';
+        responseJSON.id = "unauthorized";
+        return respondJSON(request, response, 401, responseJSON);
+      }
+      return respondJSON(request, response, 200, responseJSON);
+    };
+
+    const forbidden = (request, response) => {
+        const responseJSON = {
+          message: 'You do not have access to this content',
+          id: 'forbidden',
+        };
+        
+        return respondJSON(request, response, 403, responseJSON);
+      };
+
+    const internal = (request, response) => {
+        const responseJSON = {
+            message: 'Internal Server Error. Something went wrong.',
+            id: 'internalServerError',
+        };
+        
+        return respondJSON(request, response, 500, responseJSON);
+        };
+    
+    const notImplemented = (request, response) => {
+        const responseJSON = {
+            message: 'A get request for this page has not been implemented yet. Check again later for updated content.',
+            id: 'notImplemented',
+        };
+        
+        return respondJSON(request, response, 501, responseJSON);
+        };
+      
+  module.exports = {
+    success,
+    badRequest,
+    notFound,
+    unauthorized,
+    forbidden,
+    internal,
+    notImplemented
+  };
